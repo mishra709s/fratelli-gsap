97dfbf43336b5d786124b30cd1bc1ffc8d151cde
mishra709s <mishra709s@gmail.com>
2025-08-18 14:37:19 +0530

    chore: comment unused imports

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
 import { useMaskSettings } from '../../constants'
-import ComingSoon from './ComingSoon'
+// import ComingSoon from './ComingSoon'
 
 const Hero = () => {
   const { initialMaskPos, initialMaskSize, maskPos, maskSize } =
@@ -33,26 +33,26 @@ const Hero = () => {
       .to('.mask-wrapper', { maskSize, ease: 'power1.inOut' }, '<')
       .to('.mask-wrapper', { maskPosition: maskPos, ease: 'power1.inOut' }, '>')
       .to('.mask-wrapper', { opacity: 0 })
-      .to(
-        '.overlay-logo',
-        {
-          opacity: 1,
-          onComplete: () => {
-            gsap.to('.overlay-logo', { opacity: 0 })
-          },
-        },
-        '<'
-      )
-      .to(
-        '.entrance-message',
-        {
-          duration: 1,
-          ease: 'power1.inOut',
-          maskImage:
-            'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)',
-        },
-        '<'
-      )
+    // .to(
+    //   '.overlay-logo',
+    //   {
+    //     opacity: 1,
+    //     onComplete: () => {
+    //       gsap.to('.overlay-logo', { opacity: 0 })
+    //     },
+    //   },
+    //   '<'
+    // )
+    // .to(
+    //   '.entrance-message',
+    //   {
+    //     duration: 1,
+    //     ease: 'power1.inOut',
+    //     maskImage:
+    //       'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)',
+    //   },
+    //   '<'
+    // )
   })
 
   return (
